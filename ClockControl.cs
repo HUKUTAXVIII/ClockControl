c0dfe1322361098cc1c2bd939f163214055ec12b
Никита Антоненко <nikita.dmitrovich@gmail.com>
2021-07-06 20:05:08 +0300

    Add ClockControl

--- ClockControl.cs
+++ ClockControl.cs
@@ -51,7 +51,6 @@ namespace ClockControl
         {
             var Graphics = this.CreateGraphics();
 
-            //Graphics.DrawImage(Image.FromFile("Clock.png"),new Rectangle(new Point(0,0),this.Size));
             Graphics.DrawLine(Pens.Black,new Point(this.Width/2,this.Height/2),new Point( (int)(this.Width / 2 + Math.Cos(SecTickCount) * this.Width / 2),(int)(this.Width / 2.4 + Math.Sin(SecTickCount) * this.Height / 2.4) ));
             Graphics.DrawLine(Pens.BlueViolet,new Point(this.Width/2,this.Height/2),new Point( (int)(this.Width / 2 + Math.Cos(MinTickCount) * this.Width / 2.8),(int)(this.Width / 2 + Math.Sin(MinTickCount) * this.Height / 2.8) ));
             Graphics.DrawLine(Pens.Red,new Point(this.Width/2,this.Height/2),new Point( (int)(this.Width / 2 + Math.Cos(HrTickCount) * this.Width / 3),(int)(this.Width / 2 + Math.Sin(HrTickCount) * this.Height /3) ));
