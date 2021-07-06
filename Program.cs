974c04fcdab792731f58278733146610f215fbba
Никита Антоненко <nikita.dmitrovich@gmail.com>
2021-07-06 16:48:11 +0300

    Init Commit

--- Program.cs
+++ Program.cs
@@ -17,6 +17,8 @@ namespace ClockControl
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+
         }
     }
 }
